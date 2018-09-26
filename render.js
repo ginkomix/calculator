f97184fb95bd5025b8f215d5df778b04925aa0d3
Гинько Михаил <accer96226@gmail.com>
2018-09-26 16:33:09 +0300

    Добавленны функции простого калькулятора

--- render.js
+++ render.js
@@ -1,9 +1,13 @@
 class Render {
     constructor(){
+        this.action = document.querySelector('#action');
         this.result = document.querySelector('#result');
     }
     setValue(value){
         this.result.innerHTML = value;
+    }
+    setAction(value = '') {
+        this.action.innerHTML = value;
     }
 }
 
--- script.js
+++ script.js
@@ -2,6 +2,8 @@ class Calculator {
     constructor() {
         this.result = 0;
         this.value = '0';
+        this.action = undefined;
+        this.newAction = false;
         this.subscribeButtons();
     }
 
@@ -13,19 +15,88 @@ class Calculator {
         switch (value) {
             case 'ac':
                 this.actionAc();
-                break;
+                render.setValue(this.value);
+                render.setAction(this.action);
+                return;
             case 'ce':
                 this.actionCe();
+                render.setValue(this.value);
+                return;
+            case '=':
+                this.actioneQually();
+                render.setValue(this.value);
+                render.setAction(this.action);
+                return;
+        }
+        if (!this.action || this.newAction) {
+            this.addAction(value);
+            return;
+        }
+        this.doAction(this.action);
+        this.addAction(value);
+    }
+
+    doAction(action) {
+        switch (action) {
+            case '/':
+                this.actionDivide(this.value);
+                break;
+            case '*':
+
+                break;
+            case '-':
+                this.actionMinus(this.value);
+
+                break;
+            case '+':
+                this.actionAdd(this.value);
                 break;
             default:
                 return;
         }
+    }
+
+    addAction(action) {
+        this.action = action;
+        this.result = this.value;
+        this.newAction =true;
+        render.setAction(this.action);
         render.setValue(this.value);
     }
 
+    actioneQually() {
+        if (this.action) {
+            this.doAction(this.action);
+            this.action = undefined;
+        }
+        this.newAction = false;
+        render.setAction(this.action);
+        render.setValue(this.value);
+
+    }
+
+    actionAdd(value) {
+        this.result = Number(this.value) + Number(this.result);
+        this.value = String(this.result);
+    }
+    
+    actionMinus(value) {
+        this.result = Number(this.result) - Number(this.value);
+        this.value = String(this.result);
+    }
+
+    actionDivide(value) {
+        this.result = Number(this.result) / Number(this.value);
+        this.value = String(this.result);
+    }
+
     actionCe() {
+        
+        if (this.newAction) {
+            this.value = '0';
+        }
         this.value = this.value.slice(0, -1);
-        if (!this.value.length) {
+        if (!this.value.length ) {
             this.value = '0';
         }
     }
@@ -33,10 +104,16 @@ class Calculator {
     actionAc() {
         this.result = 0;
         this.value = '0';
+        this.newAction = false;
+        this.action = undefined;
     }
 
     addTemporaryNumber(value) {
         if (Number(this.value) === 0) {
+            this.value = '';
+        }
+        if (this.newAction) {
+            this.newAction = false;
             this.value = '';
         }
         const temporary = this.value + value;
